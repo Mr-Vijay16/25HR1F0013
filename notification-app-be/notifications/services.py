67904ffb99a24de2ee6049ee38d7ed759541f0d4
Mr-Vijay16 <hsvijay264737@gmail.com>
2026-06-27 13:58:15 +0530

    Stage 4 completed

--- notification-app-be/notifications/services.py
+++ notification-app-be/notifications/services.py
@@ -1,16 +1,19 @@
 from .models import Notification
+from django.utils import timezone
 
 
 class NotificationService:
-    @staticmethod
-    def create_notification(notification_type, message, timestamp):
-        notification = Notification.objects.create(
-            type=notification_type,
-            message=message,
-            timestamp=timestamp
-        )
-        return notification
 
     @staticmethod
-    def get_all_notifications():
-        return Notification.objects.all().order_by("-timestamp")
+    def send_notification(notification):
+
+        # Simulation only
+        print("Sending Notification...")
+        print(f"Type: {notification.type}")
+        print(f"Message: {notification.message}")
+
+        notification.status = "Sent"
+        notification.timestamp = timezone.now()
+        notification.save()
+
+        return notification
